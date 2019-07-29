83541f3e095ec1ef4d512798619f0b45f6dd2fad
Amit Singh <singhamit089@gmail.com>
2019-07-29 20:21:03 +0530

    Initial commit with project setup

---
+++ MyTaxiCodingTest/MyTaxiCodingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTaxiCodingTest
+//
+//  Created by Amit  Singh on 29/07/19.
+//  Copyright © 2019 singhamit089. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyTaxiCodingTest/Pods/Target Support Files/Pods-MyTaxiCodingTestTests/Pods-MyTaxiCodingTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTaxiCodingTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyTaxiCodingTestTests
+@end
